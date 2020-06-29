628135a59d82931deeec2c22fbe14cf36157bb77
Adam Bromiley <adam@bromiley.co.uk>
2020-06-29 19:40:02 +0100

    replace const char * args with char *

--- include/parser.h
+++ include/parser.h
@@ -60,13 +60,14 @@ extern const complex CMPLX_MIN;
 extern const complex CMPLX_MAX;
 
 
-ParseErr stringToULong(unsigned long *x, const char *nptr, unsigned long min, unsigned long max, char **endptr,
-                          int base);
-ParseErr stringToUIntMax(uintmax_t *x, const char *nptr, uintmax_t min, uintmax_t max, char **endptr, int base);
-ParseErr stringToDouble(double *x, const char *nptr, double min, double max, char **endptr);
+ParseErr stringToULong(unsigned long *x, char *nptr, unsigned long min, unsigned long max, char **endptr, int base);
+ParseErr stringToUIntMax(uintmax_t *x, char *nptr, uintmax_t min, uintmax_t max, char **endptr, int base);
+ParseErr stringToDouble(double *x, char *nptr, double min, double max, char **endptr);
 ParseErr stringToComplexPart(complex *z, char *nptr, complex min, complex max, char **endptr, ComplexPt *type);
 ParseErr stringToComplex(complex *z, char *nptr, complex min, complex max, char **endptr);
 ParseErr stringToMemory(size_t *bytes, char *nptr, size_t min, size_t max, char **endptr, int magnitude);
+
+size_t strncpyGraph(char *dest, const char *src, size_t n);
 
 
 #endif
